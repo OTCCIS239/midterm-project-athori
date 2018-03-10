bd84c3760c152a3d6823f211cd298bbdda736cc9
jstass <jasonstass@gmail.com>
2018-03-09 19:00:27 -0600

    another test

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<?php
 
-// This file initializes some goodies that will make your
-// development experience nicer! If your PHP throws an
-// error, we will show you exactly what went wrong!
-// This line should be on every page you create.
- require_once('./includes/init.php');
- require_once('./includes/db.php')
-
-// Here you might connect to the database and show off some of your newest guitars.
-
-?>
 <!doctype html>
 <html lang="en">
   <head>
